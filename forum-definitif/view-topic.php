6e61b907b4eaf944c80feed6598fc2fb18289635
JbDruet <jb.druet@codeur.online>
2021-07-02 13:37:34 +0200

    update

--- forum-definitif/view-topic.php
+++ forum-definitif/view-topic.php
@@ -5,7 +5,6 @@ require_once('db-connect.php');
 if(isset($_GET['id'])&&!empty($_GET['id'])){
     $id=strip_tags($_GET['id']);
     $sql = 'SELECT * FROM `topics` WHERE `topic_id`=:id';
-
     $query = $db->prepare($sql);
     $query->bindValue(':id', $id, PDO::PARAM_STR);
     $query ->execute();
