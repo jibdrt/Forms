6a34b8c2cdda464e905ab73546dff7c4c15b97f4
JbDruet <jb.druet@codeur.online>
2021-07-02 12:00:20 +0200

    forumCorrection

---
+++ forum-definitif/view-topic.php
@@ -0,0 +1,18 @@
+<?php 
+session_start();
+include('header.php');
+require_once('db-connect.php');
+if(isset($_GET['id'])&&!empty($_GET['id'])){
+    $id=strip_tags($_GET['id']);
+    $sql = 'SELECT * FROM `topics` WHERE `topic_id`=:id';
+
+    $query = $db->prepare($sql);
+    $query->bindValue(':id', $id, PDO::PARAM_STR);
+    $query ->execute();
+    $topic = $query->fetch();
+} else {
+    $_SESSION['error']='Je ne sais pas où vous voulez aller!';
+    header('Location:index.php');
+}
+
+?>
